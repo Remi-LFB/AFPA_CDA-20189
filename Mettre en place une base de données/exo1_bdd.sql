0056c6c7ed530b2d170b200368d4a4fa9f717463
Rémi LFB <remi.lfb@gmail.com>
2022-04-26 08:57:25 +0200

    Réalisation de l'exercice 1 sur la mise en place de BDD

---
+++ Mettre en place une base de données/exo1_bdd.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS exo1_bdd;
+CREATE DATABASE exo1_bdd;
+
+CREATE TABLE Personne(
+    per_num INT AUTO_INCREMENT,
+    per_nom VARCHAR(30) NOT NULL,
+    per_prenom VARCHAR(30) NOT NULL,
+    per_adresse VARCHAR(255) NOT NULL,
+    per_ville VARCHAR(50) NOT NULL,
+    PRIMARY KEY(per_num)
+);
+
+CREATE TABLE Groupe(
+    gro_num INT AUTO_INCREMENT,
+    gro_libelle VARCHAR(50) NOT NULL,
+    PRIMARY KEY(gro_num)
+);
+
+CREATE TABLE Appartient(
+    per_num INT,
+    gro_num INT,
+    PRIMARY KEY(per_num, gro_num),
+    FOREIGN KEY(per_num) REFERENCES Personne(per_num),
+    FOREIGN KEY(gro_num) REFERENCES Groupe(gro_num)
+);
